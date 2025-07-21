25e05957607b757795a2932cd6414a705e8f7c67
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-07-21 15:24:41 +0100

    fix image placeholder e card disposition content

--- src/Entity/SemanticVariable.php
+++ src/Entity/SemanticVariable.php
@@ -15,7 +15,7 @@ class SemanticVariable {
       'element_entity' => t('Entity'),
       'element_attribute' => t('Attribute'),
     ];
-  
+
   }
 
   public static function generateOutput($list) {
@@ -45,10 +45,50 @@ class SemanticVariable {
       $root_url = \Drupal::request()->getBaseUrl();
       $encodedUri = rawurlencode(rawurlencode($element->uri));
       $output[$element->uri] = [
-        'element_uri' => t('<a href="'.$root_url.REPGUI::DESCRIBE_PAGE.base64_encode($uri).'">'.$uri.'</a>'),     
-        'element_name' => t($label),    
+        'element_uri' => t('<a href="'.$root_url.REPGUI::DESCRIBE_PAGE.base64_encode($uri).'">'.$uri.'</a>'),
+        'element_name' => t($label),
         'element_entity' => $entityUri,
-        'element_attribute' => $attributeUri, 
+        'element_attribute' => $attributeUri,
+      ];
+    }
+    return $output;
+
+  }
+
+  public static function generateCardOutput($list) {
+
+    // ROOT URL
+    $root_url = \Drupal::request()->getBaseUrl();
+
+    $output = array();
+    foreach ($list as $element) {
+      $uri = ' ';
+      if ($element->uri != NULL) {
+        $uri = $element->uri;
+      }
+      $uri = Utils::namespaceUri($uri);
+      $label = ' ';
+      if ($element->label != NULL) {
+        $label = $element->label;
+      }
+      $entityUri = ' ';
+      if ($element->entityUri != NULL && $element->entityUri != '') {
+        $entityUri = $element->entityLabel . ' (' . Utils::namespaceUri($element->entityUri) . ')';
+      }
+      $attributeUri = ' ';
+      if ($element->attributeUri != NULL && $element->attributeUri != '') {
+        $attributeUri = $element->attributeLabel . ' (' . Utils::namespaceUri($element->attributeUri) . ')';
+      }
+      // dpm($element);
+      $root_url = \Drupal::request()->getBaseUrl();
+      $encodedUri = rawurlencode(rawurlencode($element->uri));
+      $output[$element->uri] = [
+        'element_uri' => t('<a href="'.$root_url.REPGUI::DESCRIBE_PAGE.base64_encode($uri).'">'.$uri.'</a>'),
+        'element_name' => t($label),
+        'element_entity' => $entityUri,
+        'element_attribute' => $attributeUri,
+        'element_image' => $element->hasImageUri,
+        'element_hascotypeuri' => $element->hascoTypeUri
       ];
     }
     return $output;
